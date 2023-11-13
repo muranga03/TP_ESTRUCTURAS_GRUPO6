737de3cf84f9100f18628ef2e32c657aae6a30d9
PedroM1812 <pmassalin@itba.edu.ar>
2023-11-13 16:49:14 -0300

    corregi un error en el anterior check numero

--- Menu.py
+++ Menu.py
@@ -10,78 +10,74 @@ clientesArchivo(listaClientes,"descarga")
 personalArchivo(listaPersonal,"descarga")
 ocupActual = ocupacion_actual()
 ocupBas , ocupMed , ocupPrem = ocupacion_segun_tipo()
-opcion = True
 
-opcion = True
-while opcion:
-    opcion = input("1: Crear cuenta \n2: Ingresar a su cuenta \n0: Salir del programa \n ->")
-    opcion = checkNro(opcion)
+menu_general = True
+while menu_general:
+    menu_general = input("1: Crear cuenta \n2: Ingresar a su cuenta \n0: Salir del programa \n ->")
+    menu_general = checkNro(menu_general)
     listaUsuarios = listaClientes + listaPersonal
 
-    if opcion == 1: #Crear cuenta
+    if menu_general == 1: #Crear cuenta
         signIn(listaClientes)
 
-    elif opcion == 2: #Ingresar a cuenta
+    elif menu_general == 2: #Ingresar a cuenta
         cuenta = logIn(listaUsuarios)
         if type(cuenta) == Cliente:
-            menu = "Cliente"
+            tipo_menu = "Cliente"
         elif type(cuenta) == Administrativo:
-            menu = "Administrador"
+            tipo_menu = "Administrador"
         elif type(cuenta) == Limpieza:
-            menu = "Limpieza"
+            tipo_menu = "Limpieza"
         elif type(cuenta) == Mantenimiento:
-            menu = "Mantenimiento"
+            tipo_menu = "Mantenimiento"
         
-    if menu == "Administrador":
-        menu = input("1: Menu Administrativo \n2: Menu Cliente \n3: Menu Limpieza \n4: Menu Mantenimiento \n0: Cerrar sesion \n ->")
-        menu = checkNro(menu)
-        while menu==1:
-            opcion = input("1: Crear una cuenta de Personal \n2: Ver porcentaje de ocupacion actual de habitaciones \n3: Ver Porcentaje de ocupacion actual de habitaciones segun su categoria \n4: Ver recaudacion de hoy \n5: Crear tarea \n6: Eliminar primer tarea \n7: Eliminar ultima queja \n0: Cerrar Menu Administrativo \n ->")
-            opcion = checkNro(opcion)
+    if tipo_menu == "Administrador":
+        menu_admin = input("1: Crear una cuenta de Personal \n2: Ver porcentaje de ocupacion actual de habitaciones \n3: Ver Porcentaje de ocupacion actual de habitaciones segun su categoria \n4: Ver recaudacion de hoy \n5: Crear tarea \n6: Eliminar primer tarea \n7: Eliminar ultima queja \n0: Cerrar Menu Administrativo \n ->")
+        menu_admin = checkNro(menu_admin,7)
             
-            if opcion == 1:
-                tipo = input("Seleccione el tipo de cuenta que desea crear:\n1: Cuenta Administrador \n2: Cuenta Limpieza \n3: Cuenta Mantenimiento \n0: Si desea regresar al menu \n ->")
-                tipo = checkNro(tipo)
-                crearPersonal(tipo,listaPersonal)
+        if menu_admin == 1:
+            tipo = input("Seleccione el tipo de cuenta que desea crear:\n1: Cuenta Administrador \n2: Cuenta Limpieza \n3: Cuenta Mantenimiento \n0: Si desea regresar al menu \n ->")
+            tipo = checkNro(tipo,3)
+            crearPersonal(tipo,listaPersonal)
 
-            elif opcion == 2:
-                print("El porcentaje de ocupacion de habitaciones actual es: " + ocupActual +'%')
+        elif menu_admin == 2:
+            print("El porcentaje de ocupacion de habitaciones actual es: " + ocupActual +'%')
 
-            elif opcion == 3:
-                print("El porcentaje de ocupacion de habitaciones de tipo basico actual es: " + ocupBas +'%' + "\nEl porcentaje de ocupacion de habitaciones de tipo medio actual es: " + ocupMed +'%' + "\nEl porcentaje de ocupacion de habitaciones de tipo medio actual es: " + ocupPrem +'%')
+        elif menu_admin == 3:
+            print("El porcentaje de ocupacion de habitaciones de tipo basico actual es: " + ocupBas +'%' + "\nEl porcentaje de ocupacion de habitaciones de tipo medio actual es: " + ocupMed +'%' + "\nEl porcentaje de ocupacion de habitaciones de tipo medio actual es: " + ocupPrem +'%')
             
-            elif opcion == 4:
-                cuenta.ver_recaudacion_diaria(hoy)
+        elif menu_admin == 4:
+            cuenta.ver_recaudacion_diaria(hoy)
             
-            elif opcion == 5:
-                tipo == input("Ingrese a que personal quiere agregarle la tarea: \n1: Administrador \n2: Limpieza \n3: Mantenimiento")
-                tipo = checkNro(tipo)
-                print("Los trabajos disponibles para el tipo de personal elegido son los siguientes: ")
-                if tipo == 1:
-                    clase =  Administrativo
-                if tipo == 2:
-                    clase = Limpieza
-                if tipo == 3:
-                    clase = Mantenimiento
-                cont=0
-                for i in clase.trabajos:
-                    print(cont + ": " + i)
-                    cont=+1
-                tarea = input("Ingrese la opcion deseada")
-                tarea = checkNro(tarea)
-                cuenta.asignar_tarea(tarea,tipo)
+        elif menu_admin == 5:
+            tipo == input("Ingrese a que personal quiere agregarle la tarea: \n1: Administrador \n2: Limpieza \n3: Mantenimiento")
+            tipo = checkNro(tipo)
+            print("Los trabajos disponibles para el tipo de personal elegido son los siguientes: ")
+            if tipo == 1:
+                clase =  Administrativo
+            if tipo == 2:
+                clase = Limpieza
+            if tipo == 3:
+                clase = Mantenimiento
+            cont=0
+            for i in clase.trabajos:
+                print(cont + ": " + i)
+                cont=+1
+            tarea = input("Ingrese la opcion deseada")
+            tarea = checkNro(tarea,len(clase.trabajos))
+            cuenta.asignar_tarea(clase.trabajos[tarea],tipo)
 
-            elif opcion == 6:
-                cuenta.borrar_primera_tarea()
+        elif menu_admin == 6:
+            cuenta.realizar_tarea()
 
-            elif opcion == 7:
-                cuenta.eliminar_ultima_queja()
+        elif menu_admin == 7:
+            cuenta.eliminar_ultima_queja()
 
-    if menu == "Mantenimiento":
+    if tipo_menu == "Mantenimiento":
         '''La funcion esta detallada en Funciones.py'''
         menu_Limpieza_Mantenimiento(cuenta)
 
-    if menu == "Mantenimiento":
+    if tipo_menu == "Mantenimiento":
         '''La funcion esta detallada en Funciones.py'''
         menu_Limpieza_Mantenimiento(cuenta)
 
